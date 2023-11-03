2d23edcfb9de7d045ddbfaa17cff6e81fe754d1a
Paul Sisson <paulsissonsemail@gmail.com>
2023-11-02 20:10:18 -0500

    Begin setup for hero content

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   env: {
     API_KEY: '580337358287',
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,45 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image'
-import styles from './page.module.css'
-import { ChangeEvent } from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
+import { ChangeEvent, useState } from 'react';
+import { Bangers } from 'next/font/google';
+import { characters } from './characterIDs.js';
+
+const HeroFont = Bangers({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+const characterNames: string[] = [];
+const getNames = characters.map((character) => {
+  characterNames.push(character.name);
+});
+
 
 export default function Home() {
-
   const router = useRouter();
 
+
+
   function handleSearchChange(e: ChangeEvent<HTMLInputElement>) {
-    router.push(`?hero=${e.target.value}`)
+    router.push(`?hero=${e.target.value}`);
   }
 
   return (
     <main>
-      <h1 className={styles.title}>Super Hero Big Book</h1>
+      <h1 className={[styles.title, HeroFont.className].join(' ')}>
+        Super Hero Big Book
+      </h1>
       <form className={styles.form}>
-        <input type='text' name='search' id='search' className={styles.search} onChange={handleSearchChange} placeholder='Search by name' />
+        <input
+          type="text"
+          name="hero"
+          id="hero"
+          className={styles.search}
+          onChange={handleSearchChange}
+          placeholder="Search by name"
+        />
         <button className={styles.searchButton}>Search Supes</button>
       </form>
       <div className={styles.book}>
@@ -25,10 +48,98 @@ export default function Home() {
         <div className={styles.page}></div>
         <div className={styles.page}></div>
         <div className={styles.page}></div>
-        <div className={styles.page}></div>
-        <div className={styles.heroPage}></div>
+        <div className={styles.page}>
+          <span className={styles.square}></span>
+        </div>
+        <div className={styles.heroPage}>
+          <Image
+            src="https://source.unsplash.com/random"
+            alt="super hero"
+            width={100}
+            height={100}
+            className={styles.heroImage}
+          />
+          <h2>Spider-man</h2>
+
+          {/* stats */}
+          <div className={styles.statsContainer}>
+            {/* Intelligence */}
+            <label className={styles.statLabel}>
+              Int
+              <div
+                className={styles.stat}
+                style={{
+                  background: `radial-gradient(circle at center, beige 0 40%, transparent 40% 100%),conic-gradient(#11378b 0deg calc(3.6 * ${75}deg), beige calc(3.6 * ${75}deg) 360deg)`,
+                }}
+              ></div>
+            </label>
+
+            {/* Strength */}
+            <label className={styles.statLabel}>
+              Str
+              <div
+                className={styles.stat}
+                style={{
+                  background: `radial-gradient(circle at center, beige 0 40%, transparent 40% 100%),conic-gradient(#741b1b 0deg calc(3.6 * ${55}deg), beige calc(3.6 * ${55}deg) 360deg)`,
+                }}
+              ></div>
+            </label>
+
+            {/* Speed */}
+            <label className={styles.statLabel}>
+              Spd
+              <div
+                className={styles.stat}
+                style={{
+                  background: `radial-gradient(circle at center, beige 0 40%, transparent 40% 100%),conic-gradient(#d5bb29 0deg calc(3.6 * ${63}deg), beige calc(3.6 * ${63}deg) 360deg)`,
+                }}
+              ></div>
+            </label>
+
+            {/* Durability */}
+            <label className={styles.statLabel}>
+              Dur
+              <div
+                className={styles.stat}
+                style={{
+                  background: `radial-gradient(circle at center, beige 0 40%, transparent 40% 100%),conic-gradient(#46525b 0deg calc(3.6 * ${70}deg), beige calc(3.6 * ${70}deg) 360deg)`,
+                }}
+              ></div>
+            </label>
+
+            {/* Power */}
+            <label className={styles.statLabel}>
+              Pwr
+              <div
+                className={styles.stat}
+                style={{
+                  background: `radial-gradient(circle at center, beige 0 40%, transparent 40% 100%),conic-gradient(#1a4222 0deg calc(3.6 * ${66}deg), beige calc(3.6 * ${66}deg) 360deg)`,
+                }}
+              >
+                66
+              </div>
+            </label>
+
+            {/* Combat */}
+            <label className={styles.statLabel}>
+              Com
+              <div
+                className={styles.stat}
+                style={{
+                  background: `radial-gradient(circle at center, beige 0 40%, transparent 42% 100%),conic-gradient(#421a42 0deg calc(3.6 * ${70}deg), beige calc(3.6 * ${70}deg) 360deg)`,
+                }}
+              ></div>
+            </label>
+          </div>
+          {/* Bio */}
+          <p>Name</p>
+          <p>Hometown</p>
+          <p>First Appearance</p>
+          <p>Publisher</p>
+          <p>Good guy / bad guy</p>
+        </div>
         <div className={styles.backCover}>Back Cover!!!</div>
       </div>
     </main>
-  )
+  );
 }
